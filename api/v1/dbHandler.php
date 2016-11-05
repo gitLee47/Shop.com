f8c949fb7ecad2e5e50603cf975fb6ed97c53596
Liju Robin George <liju.leelives@gmail.com>
2016-11-05 18:56:38 -0400

    initial commit

---
+++ api/v1/dbHandler.php
@@ -0,0 +1,243 @@
+<?php
+
+class DbHandler {
+
+    private $conn;
+
+    function __construct() {
+        require_once 'dbConnect.php';
+        // opening db connection
+        $db = new dbConnect();
+        $this->conn = $db->connect();
+    }
+    /**
+     * Fetching single record
+     */
+    public function getOneRecord($query) {
+        $r = $this->conn->query($query.' LIMIT 1') or die($this->conn->error.__LINE__);
+        return $result = $r->fetch_assoc();    
+    }
+    /**
+     * Creating new record
+     */
+    public function insertIntoTable($obj, $column_names, $table_name) {
+        
+        $c = (array) $obj;
+        $keys = array_keys($c);
+        $columns = '';
+        $values = '';
+        foreach($column_names as $desired_key){ // Check the obj received. If blank insert blank into the array.
+           if(!in_array($desired_key, $keys)) {
+                $$desired_key = '';
+            }else{
+                $$desired_key = $c[$desired_key];
+            }
+            $columns = $columns.$desired_key.',';
+            $values = $values."'".$$desired_key."',";
+        }
+        $query = "INSERT INTO ".$table_name."(".trim($columns,',').") VALUES(".trim($values,',').")";
+        $r = $this->conn->query($query) or die($this->conn->error.__LINE__);
+
+        if ($r) {
+            $new_row_id = $this->conn->insert_id;
+            return $new_row_id;
+            } else {
+            return NULL;
+        }
+    }
+	
+/*New Functions */
+
+function select($table, $columns, $where){
+        try{
+            $a = array();
+            $w = "";
+            foreach ($where as $key => $value) {
+                $w .= " and " .$key. " like :".$key;
+                $a[":".$key] = $value;
+            }
+            $stmt = $this->db->prepare("select ".$columns." from ".$table." where 1=1 ". $w);
+            $stmt->execute($a);
+            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            if(count($rows)<=0){
+                $response["status"] = "warning";
+                $response["message"] = "No data found.";
+            }else{
+                $response["status"] = "success";
+                $response["message"] = "Data selected from database";
+            }
+                $response["data"] = $rows;
+        }catch(PDOException $e){
+            $response["status"] = "error";
+            $response["message"] = 'Select Failed: ' .$e->getMessage();
+            $response["data"] = null;
+        }
+        return $response;
+    }
+	
+    function select2($table, $columns, $where, $order){
+        try{
+            $a = array();
+            $w = "";
+            foreach ($where as $key => $value) {
+                $w .= " and " .$key. " like :".$key;
+                $a[":".$key] = $value;
+            }
+            $stmt = $this->db->prepare("select ".$columns." from ".$table." where 1=1 ". $w." ".$order);
+            $stmt->execute($a);
+            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            if(count($rows)<=0){
+                $response["status"] = "warning";
+                $response["message"] = "No data found.";
+            }else{
+                $response["status"] = "success";
+                $response["message"] = "Data selected from database";
+            }
+                $response["data"] = $rows;
+        }catch(PDOException $e){
+            $response["status"] = "error";
+            $response["message"] = 'Select Failed: ' .$e->getMessage();
+            $response["data"] = null;
+        }
+        return $response;
+    }
+	
+    function insert($table, $columnsArray, $requiredColumnsArray) {
+        $this->verifyRequiredParams($columnsArray, $requiredColumnsArray);
+        
+        try{
+            $a = array();
+            $c = "";
+            $v = "";
+            foreach ($columnsArray as $key => $value) {
+                $c .= $key. ", ";
+                $v .= ":".$key. ", ";
+                $a[":".$key] = $value;
+            }
+            $c = rtrim($c,', ');
+            $v = rtrim($v,', ');
+            $stmt =  $this->db->prepare("INSERT INTO $table($c) VALUES($v)");
+            $stmt->execute($a);
+            $affected_rows = $stmt->rowCount();
+            $lastInsertId = $this->db->lastInsertId();
+            $response["status"] = "success";
+            $response["message"] = $affected_rows." row inserted into database";
+            $response["data"] = $lastInsertId;
+        }catch(PDOException $e){
+            $response["status"] = "error";
+            $response["message"] = 'Insert Failed: ' .$e->getMessage();
+            $response["data"] = 0;
+        }
+        return $response;
+    }
+	
+    function update($table, $columnsArray, $where, $requiredColumnsArray){ 
+        $this->verifyRequiredParams($columnsArray, $requiredColumnsArray);
+        try{
+            $a = array();
+            $w = "";
+            $c = "";
+            foreach ($where as $key => $value) {
+                $w .= " and " .$key. " = :".$key;
+                $a[":".$key] = $value;
+            }
+            foreach ($columnsArray as $key => $value) {
+                $c .= $key. " = :".$key.", ";
+                $a[":".$key] = $value;
+            }
+                $c = rtrim($c,", ");
+
+            $stmt =  $this->db->prepare("UPDATE $table SET $c WHERE 1=1 ".$w);
+            $stmt->execute($a);
+            $affected_rows = $stmt->rowCount();
+            if($affected_rows<=0){
+                $response["status"] = "warning";
+                $response["message"] = "No row updated";
+            }else{
+                $response["status"] = "success";
+                $response["message"] = $affected_rows." row(s) updated in database";
+            }
+        }catch(PDOException $e){
+            $response["status"] = "error";
+            $response["message"] = "Update Failed: " .$e->getMessage();
+        }
+        return $response;
+    }
+	
+    function delete($table, $where){
+        if(count($where)<=0){
+            $response["status"] = "warning";
+            $response["message"] = "Delete Failed: At least one condition is required";
+        }else{
+            try{
+                $a = array();
+                $w = "";
+                foreach ($where as $key => $value) {
+                    $w .= " and " .$key. " = :".$key;
+                    $a[":".$key] = $value;
+                }
+                $stmt =  $this->db->prepare("DELETE FROM $table WHERE 1=1 ".$w);
+                $stmt->execute($a);
+                $affected_rows = $stmt->rowCount();
+                if($affected_rows<=0){
+                    $response["status"] = "warning";
+                    $response["message"] = "No row deleted";
+                }else{
+                    $response["status"] = "success";
+                    $response["message"] = $affected_rows." row(s) deleted from database";
+                }
+            }catch(PDOException $e){
+                $response["status"] = "error";
+                $response["message"] = 'Delete Failed: ' .$e->getMessage();
+            }
+        }
+        return $response;
+    }
+
+/*End New Functions*/
+	
+public function getSession(){
+    if (!isset($_SESSION)) {
+        session_start();
+    }
+    $sess = array();
+    if(isset($_SESSION['uid']))
+    {
+        $sess["uid"] = $_SESSION['uid'];
+        $sess["name"] = $_SESSION['name'];
+        $sess["email"] = $_SESSION['email'];
+    }
+    else
+    {
+        $sess["uid"] = '';
+        $sess["name"] = 'Guest';
+        $sess["email"] = '';
+    }
+    return $sess;
+}
+public function destroySession(){
+    if (!isset($_SESSION)) {
+    session_start();
+    }
+    if(isSet($_SESSION['uid']))
+    {
+        unset($_SESSION['uid']);
+        unset($_SESSION['name']);
+        unset($_SESSION['email']);
+        $info='info';
+        if(isSet($_COOKIE[$info]))
+        {
+            setcookie ($info, '', time() - $cookie_time);
+        }
+        $msg="Logged Out Successfully...";
+    }
+    else
+    {
+        $msg = "Not logged in...";
+    }
+    return $msg;
+}
+ 
+}
+
+?>
---
+++ js/app.js
@@ -0,0 +1,107 @@
+'use strict';
+var storeApp = angular.module('AngularStore', ['ngRoute', 'ngAnimate', 'toaster']);
+// App Module: the name AngularStore matches the ng-app attribute in the main <html> tag
+// the route provides parses the URL and injects the appropriate partial page
+storeApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+		when('/store', {
+        templateUrl: 'partials/store.htm',
+        controller: storeController 
+		}).
+		when('/products/:productSku', {
+        templateUrl: 'partials/product.htm',
+        controller: storeController
+		}).
+		when('/cart', {
+        templateUrl: 'partials/shoppingCart.htm',
+        controller: storeController
+		}).
+		when('/login', {
+            title: 'Login',
+            templateUrl: 'partials/login.html',
+            controller: 'authCtrl'
+        }).
+		when('/logout', {
+                title: 'Logout',
+                templateUrl: 'partials/login.html',
+                controller: 'logoutCtrl'
+        }).
+		when('/signup', {
+                title: 'Signup',
+                templateUrl: 'partials/signup.html',
+                controller: 'authCtrl'
+            }).
+		when('/dashboard', {
+                title: 'Dashboard',
+                templateUrl: 'partials/dashboard.html',
+                controller: 'authCtrl'
+            }).
+		when('/', {
+                title: 'Login',
+                templateUrl: 'partials/login.html',
+                controller: 'authCtrl',
+                role: '0'
+            }).
+      otherwise({
+        redirectTo: '/login'
+      });
+}]).run(function ($rootScope, $location, Data) {
+        $rootScope.$on("$routeChangeStart", function (event, next, current) {
+            $rootScope.authenticated = false;
+            Data.get('session').then(function (results) {
+                if (results.uid) {
+                    $rootScope.authenticated = true;
+                    $rootScope.uid = results.uid;
+                    $rootScope.name = results.name;
+                    $rootScope.email = results.email;
+                } else {
+                    var nextUrl = next.$$route.originalPath;
+                    if (nextUrl == '/signup' || nextUrl == '/login') {
+
+                    } else {
+                        $location.path("/login");
+                    }
+                }
+            });
+        });
+    });
+
+// create a data service that provides a store and a shopping cart that
+// will be shared by all views (instead of creating fresh ones for each view).
+storeApp.factory("DataService", function () {
+
+    // create store
+    var myStore = new store();
+
+    // create shopping cart
+    var myCart = new shoppingCart("AngularStore");
+
+    // enable PayPal checkout
+    // note: the second parameter identifies the merchant; in order to use the 
+    // shopping cart with PayPal, you have to create a merchant account with 
+    // PayPal. You can do that here:
+    // https://www.paypal.com/webapps/mpp/merchant
+    myCart.addCheckoutParameters("PayPal", "bernardo.castilho-facilitator@gmail.com");
+
+    // enable Google Wallet checkout
+    // note: the second parameter identifies the merchant; in order to use the 
+    // shopping cart with Google Wallet, you have to create a merchant account with 
+    // Google. You can do that here:
+    // https://developers.google.com/commerce/wallet/digital/training/getting-started/merchant-setup
+    myCart.addCheckoutParameters("Google", "500640663394527",
+        {
+            ship_method_name_1: "UPS Next Day Air",
+            ship_method_price_1: "20.00",
+            ship_method_currency_1: "USD",
+            ship_method_name_2: "UPS Ground",
+            ship_method_price_2: "15.00",
+            ship_method_currency_2: "USD"
+        }
+    );
+
+    // return data object with store and cart
+    return {
+        store: myStore,
+        cart: myCart
+    };
+});

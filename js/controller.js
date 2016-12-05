2c8d7230cc55b931ed9cf9b50171c1b48d9419b5
Liju Robin George <liju.leelives@gmail.com>
2016-12-04 21:53:02 -0500

    improved look and feel

--- js/controller.js
+++ js/controller.js
@@ -8,6 +8,7 @@ storeApp.controller('storeController', function($scope, $rootScope, $modal, $rou
     // get store and cart from service
     $scope.store = DataService.store;
     $scope.cart = DataService.cart;
+	
 
     // use routing to pick the selected product
     //if ($routeParams.productSku != null) {
